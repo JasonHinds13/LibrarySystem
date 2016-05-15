27e672c06fc1a4880cb7e51f463ce0499fdefadd
Brandon Hinds <brandonhinds13@gmail.com>
2016-05-15 19:41:38 -0400

    Add files via upload

---
+++ LibrarySystem.h
@@ -0,0 +1,126 @@
+#include <iostream>
+
+using namespace std;
+
+//Book class
+class Book
+{
+private:
+
+    int ISBN; //The ISBN Of The Book
+    string name; //The Name Of The Book
+    string author; //The Author Of The Book
+    string publisher; //The Publisher Of The Book
+
+public:
+
+    void setISBN(int isbn)
+    {
+        ISBN = isbn;
+    }
+
+    void setName(string nam)
+    {
+        name = nam;
+    }
+
+    void setAuthor(string auth)
+    {
+        author = auth;
+    }
+
+    void setPublisher(string pub)
+    {
+        publisher = pub;
+    }
+
+    int getISBN()
+    {
+        return ISBN;
+    }
+
+    string getName()
+    {
+        return name;
+    }
+
+    string getAuthor()
+    {
+        return author;
+    }
+
+    string getPublisher()
+    {
+        return publisher;
+    }
+
+};
+
+//student class
+class Student
+{
+private:
+    string name; //name of student
+    string Class; //class student is in
+    Book book; //book the student borrowed
+
+public:
+
+    void setName(string myname)
+    {
+        name = myname;
+    }
+
+    void setClass(string myclass)
+    {
+        Class = myclass;
+    }
+
+    void setBook(Book bk)
+    {
+        book = bk;
+    }
+
+    string getName()
+    {
+        return name;
+    }
+
+    string getClass()
+    {
+        return Class;
+    }
+
+    Book getBook()
+    {
+        return book;
+    }
+};
+
+
+//functions
+Book makeBook(int ISBN, string name, string author, string publisher);
+Student makeStudent(string name, string Class, Book book);
+
+Book makeBook(int ISBN, string name, string author, string publisher)
+{
+    Book newbook;
+
+    newbook.setISBN(ISBN);
+    newbook.setName(name);
+    newbook.setAuthor(author);
+    newbook.setPublisher(publisher);
+
+    return newbook;
+}
+
+Student makeStudent(string name, string Class, Book book)
+{
+    Student student;
+
+    student.setName(name);
+    student.setClass(Class);
+    student.setBook(book);
+
+    return student;
+}
---
+++ NewLibrarySystem.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "LibrarySystem.h"
+
+int main(int argc, char**argv)
+{
+    Book book = makeBook(1220, "Adventures", "Matthew", "Penguin");
+    Student student = makeStudent("Adrian", "5T", book);
+
+    cout<<"\t\t\tBook Details\n";
+    cout<<"------------------------------------------\n\n";
+    cout<<"ISBN:      "<<book.getISBN()<<"\n";
+    cout<<"Name:      "<<book.getName()<<"\n";
+    cout<<"Author:    "<<book.getAuthor()<<"\n";
+    cout<<"Publisher: "<<book.getPublisher()<<"\n";
+    cout<<"Student:   "<<student.getName()<<"\n";
+    cout<<"Class:     "<<student.getClass()<<"\n";
+
+    return 0;
+}
