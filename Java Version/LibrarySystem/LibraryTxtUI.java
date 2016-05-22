58f6e720c6f5456429ea15d38bd033d96dd68946
Brandon Hinds <brandonhinds13@gmail.com>
2016-05-21 20:53:05 -0400

    Update LibraryTxtUI.java

--- Java Version/LibrarySystem/LibraryTxtUI.java
+++ Java Version/LibrarySystem/LibraryTxtUI.java
@@ -25,9 +25,10 @@ public class LibraryTxtUI
             System.out.println("\nChoose An Option Below: ");
             System.out.println("A. Loan A Book");
             System.out.println("B. Find A Loaned Book");
-            System.out.println("C. Return A Loaned Book");
-            System.out.println("D. Display All Books on Loan");
-            System.out.println("E. To Exit");
+            System.out.println("C. Update A Student");
+            System.out.println("D. Return A Loaned Book");
+            System.out.println("E. Display All Books on Loan");
+            System.out.println("F. To Exit");
             
             System.out.println("Option: ");
             option = scan.nextLine();
@@ -66,6 +67,28 @@ public class LibraryTxtUI
             }
             else if(option.equals("C"))
             {
+                String sName,Class; //Student Info
+                String ISBN, bName, author, publisher; //Book Info
+                
+                System.out.print("Enter Student Name: ");
+                sName = scan.nextLine();
+                System.out.print("Enter Students Class: ");
+                Class = scan.nextLine();
+                System.out.print("Enter Book ISBN: ");
+                ISBN = scan.nextLine();
+                System.out.print("Enter Book Name: ");
+                bName = scan.nextLine();
+                System.out.print("Enter Book Author: ");
+                author = scan.nextLine();
+                System.out.print("Enter Book Publisher: ");
+                publisher = scan.nextLine();
+                
+                Book book = new Book(ISBN, bName, author, publisher);
+                
+                manager.updateStudent(new Student(sName, Class, book));
+            }
+            else if(option.equals("D"))
+            {
                 String sName;
                 
                 System.out.print("Enter Student's Name To Remove A Book: ");
@@ -73,7 +96,7 @@ public class LibraryTxtUI
                 
                 manager.removeStudent(sName);
             }
-            else if(option.equals("D"))
+            else if(option.equals("E"))
             {
                 for(Student student: manager.getStudents())
                 {
@@ -82,7 +105,7 @@ public class LibraryTxtUI
                     System.out.println("---------------------------");
                 }
             }
-            else if(option.equals("E"))
+            else if(option.equals("F"))
             {
                 running = false;
                 System.out.println("Goodbye!");
